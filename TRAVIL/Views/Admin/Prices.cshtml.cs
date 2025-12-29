7cdd4f0d0a95e935243afab9367e0e2fb4f9fcc1
Islam Mhareeq <islammh@ac.sce.ac.il>
2025-12-29 23:39:35 +0200

    styling

---
+++ TRAVIL/Views/Admin/Prices.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TRAVEL.Views.Admin
+{
+    public class PricesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
